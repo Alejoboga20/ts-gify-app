251a82e90fc6e83f990b63de72e29a39e7906d51
Alejo Boga <alejoboga20@gmail.com>
2023-05-17 10:53:38 -0400

    add gif categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { SearchBar } from '@/components';
+import { useState } from 'react';
+
+import { GifGrid, SearchBar } from '@/components';
 import { BaseLayout } from '@/layout';
 
 export const App = () => {
+  const [categories, setCategories] = useState<string[]>([]);
+
+  const onAddNewCategory = (category: string) => {
+    if (categories.includes(category)) return;
+
+    setCategories((prevCat) => [category, ...prevCat]);
+  };
+
   return (
     <BaseLayout>
-      <SearchBar />
+      <SearchBar onAddNewCategory={onAddNewCategory} />
+      {categories.map((category) => (
+        <GifGrid key={category} gifCategory={category} />
+      ))}
     </BaseLayout>
   );
 };
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,37 @@
-import { useId } from 'react';
+import { useId, useState } from 'react';
 
-export const SearchBar = () => {
+interface SearchBarProps {
+  onAddNewCategory: (category: string) => void;
+}
+
+const INITIAL_SEARCH = '';
+
+export const SearchBar = ({ onAddNewCategory }: SearchBarProps) => {
   const id = useId();
+  const [gifCategory, setGifCategory] = useState(INITIAL_SEARCH);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    onAddNewCategory(gifCategory);
+    setGifCategory(INITIAL_SEARCH);
+  };
 
   return (
-    <form className='flex flex-col gap-2 sm:flex-row'>
+    <form className='flex flex-col gap-2 sm:flex-row' onSubmit={onSubmit}>
       <label htmlFor={id} className='sr-only' />
       <input
         id='id'
         className='w-full rounded border-2 border-neutral-200 p-2 text-center sm:text-left'
         placeholder='Search Gifs'
+        name='gif-search'
+        value={gifCategory}
+        onChange={(e) => setGifCategory(e.target.value)}
+        autoComplete='off'
+        spellCheck={false}
         tabIndex={1}
+        type='search'
+        autoFocus
       />
       <button className='btn-primary w-full sm:w-fit' type='submit' tabIndex={2}>
         Search
