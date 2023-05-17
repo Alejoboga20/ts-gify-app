082e1997587206087ee5fe13848298db0f9827d4
Alejo Boga <60139228+Alejoboga20@users.noreply.github.com>
2023-05-17 09:47:03 -0400

    Install Tailwind, Prettier and Linter Config (#1)
    * install tailwind
    
    * add prettier plugins for tailwind
    
    * config prettier and linter
    
    * config PR templates and codeowners
    
    * extending tailwind colors
    
    * add husky
    
    * add husky
    
    * add husky
    
    * lint command

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  define: { global: {} },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
+});
